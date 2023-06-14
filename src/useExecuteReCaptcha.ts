0be27d2d0d5dcf611a787c30492a6de48818e041
bladl <vladgadzyra@gmail.com>
2023-06-14 23:58:14 +0300

    `useExecuteReCaptcha` hook as the main entry for events. Asynchronous recaptcha actions queueing.

---
+++ src/useExecuteReCaptcha.ts
@@ -0,0 +1,11 @@
+import {useContext} from "react";
+import {Context} from "./RecaptchaProvider";
+
+const useExecuteReCaptcha = ()=>{
+    return useContext(Context)??{
+        useExecuteReCaptcha:()=>{
+            console.warn('Recaptcha context not injected.')
+        }
+    }
+}
+export default useExecuteReCaptcha
