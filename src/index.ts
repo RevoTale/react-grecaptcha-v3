e1713fadcfdeaef0543982e8552629243e355198
bladl <vladgadzyra@gmail.com>
2023-06-14 23:05:22 +0300

    add an option to disable recaptcha badge

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import RecaptchaProvider, {Props as RecaptchaProviderProps} from './RecaptchaProvider'
 
 export type {RecaptchaProviderProps}
+
 export {RecaptchaProvider,}
+export {default, reCaptchaHiddenBadgeStyles} from './useHideRecaptchaBadge';
