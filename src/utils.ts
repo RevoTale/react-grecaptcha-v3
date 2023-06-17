15ee285ba2241fe8b38be0e8714315327b1d3613
bladl <vladgadzyra@gmail.com>
2023-06-17 16:45:08 +0300

    fix appendTo

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,19 @@
-
 export type ReCaptchaProps = Readonly<{
-    siteKey: string, useRecaptchaNet:boolean,enterprise:boolean
+    siteKey: string, useRecaptchaNet: boolean, enterprise: boolean
 }>
-export const getScriptSrc = ({enterprise,useRecaptchaNet,siteKey}:ReCaptchaProps):string=>{
+export const getScriptSrc = ({enterprise, useRecaptchaNet, siteKey}: ReCaptchaProps): string => {
     const hostname = useRecaptchaNet ? "recaptcha.net" : "www.google.com";
     if (enterprise) {
         return `https://${hostname}/recaptcha/enterprise.js?render=explicit`;
     }
     return `https://${hostname}/recaptcha/api.js?render=${siteKey}`
 }
-export const prepareGlobalObject =()=>{
-    if(typeof window.grecaptcha === 'undefined') {
+export const prepareGlobalObject = () => {
+    if (typeof window.grecaptcha === 'undefined') {
         window.grecaptcha = {
-        
-            ready(cb){
-                if(typeof window.grecaptcha === 'undefined') {
+
+            ready(cb) {
+                if (typeof window.grecaptcha === 'undefined') {
                     // window.__grecaptcha_cfg is a global variable that stores reCAPTCHA's
                     // configuration. By default, any functions listed in its 'fns' property
                     // are automatically executed when reCAPTCHA loads.
@@ -22,7 +21,7 @@ export const prepareGlobalObject =()=>{
                     // @ts-ignore
                     window[c] = window[c] || {};
                     // @ts-ignore
-                    (window[c]['fns'] = window[c]['fns']||[]).push(cb);
+                    (window[c]['fns'] = window[c]['fns'] || []).push(cb);
                 } else {
                     cb();
                 }
@@ -31,27 +30,27 @@ export const prepareGlobalObject =()=>{
     }
 }
 export type CreatScriptProps = Readonly<{
-    id:string
-    async:boolean
-    defer:boolean
-    src:string
-    nonce?:string
+    id: string
+    async: boolean
+    defer: boolean
+    src: string
+    nonce?: string
 }>
-export const createScript = (props:CreatScriptProps): HTMLScriptElement => {
-    const el = document.createElement('script')
-    Object.assign(el,props)
-    return el
-}
 
-export type InjectScriptProps = CreatScriptProps&{ appendTo: 'head' | 'body';}
-export const maybeInjectScript = (props:InjectScriptProps): HTMLScriptElement => {
-    const el = document.querySelector(`script[id=${props.id}]`)
-    if (null === el) {
-        return document.head.appendChild(createScript(props))
+export type InjectScriptProps = CreatScriptProps & { appendTo: 'head' | 'body'; }
+export const maybeInjectScript = ({appendTo,...scriptProps}: InjectScriptProps): HTMLScriptElement => {
+    const el = document.querySelector(`script[id=${scriptProps.id}]`)
+    if (el === null) {
+        const el = document.createElement('script') as HTMLScriptElement
+        Object.assign(el, scriptProps);
+        (appendTo === 'head'?document.head:document.body).appendChild(el)
+        return el
     }
+    Object.assign(el, scriptProps)
+
     return el as HTMLScriptElement
 }
-export const maybeRemoveScript = (scriptId: string):void => {
+export const maybeRemoveScript = (scriptId: string): void => {
     const el = document.querySelector(`script[id=${scriptId}]`)
     if (null !== el) {
         document.head.removeChild(el)
