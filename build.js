db2e96b13b578675eb322b588f058e110ad891ac
bladl <vladgadzyra@gmail.com>
2023-06-28 11:29:16 +0300

    Adjust esbuild config to support some browsers, and tree shaking

--- build.js
+++ build.js
@@ -1,32 +1,48 @@
 import { build } from 'esbuild';
-import config from './package.json' assert { type: 'json' };
 import rimraf from 'rimraf';
-const { dependencies } = config;
-const entryFile = 'src/index.ts';
+const target = ['chrome64', 'edge79', 'firefox67', 'node16', 'safari11'];
+import glob from 'resolve-glob';
+const entryPoints = glob.sync([
+  './src/**/*.ts',
+  './src/**/*.tsx',
+  './src/**/*.js',
+  './src/**/*.jsx',
+]);
+const shared = {
+  entryPoints,
+  // Treat all dependencies in package.json as externals to keep bundle size to a minimum
+  //bundle: true,
+  packages: 'external',
+  logLevel: 'info',
+  bundle: false,
+  minify: true,
+  platform: 'neutral',
+
+  treeShaking: true,
+  sourcemap: true,
+};
 const handleBuild = async () => {
   await rimraf('./dist');
-  const shared = {
-    bundle: true,
-    entryPoints: [entryFile],
-    // Treat all dependencies in package.json as externals to keep bundle size to a minimum
-    external: Object.keys(dependencies),
-    logLevel: 'info',
-    minify: true,
-    sourcemap: true,
-  };
 
   void build({
     ...shared,
     format: 'esm',
-    outfile: './dist/index.esm.js',
-    target: ['esnext', 'node12.22.0'],
+    //splitting: true,
+    outExtension: {
+      '.js': '.mjs',
+    },
+    outdir: './dist/esm/',
+    target,
   });
 
   void build({
     ...shared,
     format: 'cjs',
-    outfile: './dist/index.cjs.js',
-    target: ['esnext', 'node12.22.0'],
+    outExtension: {
+      '.js': '.cjs',
+    },
+    outdir: './dist/cjs/',
+    target,
   });
 };
 void handleBuild();
