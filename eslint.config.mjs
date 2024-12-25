82001455d06ad80242508db8fcde56cb96f912c4
l-you <l-you@revotale.com>
2024-12-25 21:56:52 +0200

    Fix eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import love from 'eslint-config-love'
+
+export default {
+    ...love,
+    files: ['./src/**/*.tsx', './src/**/*.ts'],
+    "ignorePatterns": [ "/__tests__/sources/**","**/dist/**"],
+}
