21bfffc00d42c9d77b5f1428384877704398790e
l-you <l-you@revotale.com>
2025-05-04 15:46:13 +0000

    fix missing eslint deps

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "eslint/config";
+
+import love from 'eslint-config-love'
+
+export default defineConfig([
+  { ...love,
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+
+  },
+]);
--- eslint.config.mjs
+++
@@ -1,6 +0,0 @@
-import love from 'eslint-config-love'
-
-export default [{
-    ...love,
-    files: ['./src/**/*.tsx', './src/**/*.ts']
-}]
