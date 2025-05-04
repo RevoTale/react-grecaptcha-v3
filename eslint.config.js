ce9ceec33258e744d3a03f1ce4f0ef016db472af
l-you <l-you@revotale.com>
2025-05-04 16:42:03 +0000

    fix eslint cofig analyzed files from dist

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from 'eslint/config'
 import love from 'eslint-config-love'
 
 export default defineConfig([
+    {
+        ignores: ['dist/**/*', '__tests__/**/*'],
+    },
     { ...love, files: ['./src/**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
 ])
