abca24d05c4514560b4c5883c521156f396ad6dd
l-you <l-you@revotale.com>
2025-05-09 09:51:47 +0000

    Change project builder to tsup

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/**/*.ts'],
+  sourcemap: true,
+  clean: true,
+  format: ['cjs', 'esm'],
+  dts: true,
+  bundle: false,
+  target: 'es5',
+})
