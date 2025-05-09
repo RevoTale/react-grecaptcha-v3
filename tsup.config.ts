ac4d194e63afeed90b7b46f70918170a8dbeba10
l-you <l-you@revotale.com>
2025-05-09 09:57:31 +0000

    fix prettier

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/**/*.ts'],
-  sourcemap: true,
-  clean: true,
-  format: ['cjs', 'esm'],
-  dts: true,
-  bundle: false,
-  target: 'es5',
+    entry: ['src/**/*.ts'],
+    sourcemap: true,
+    clean: true,
+    format: ['cjs', 'esm'],
+    dts: true,
+    bundle: false,
+    target: 'es5',
 })
